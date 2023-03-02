0c8507a5baec3862775ec77145828cdfd9a79e14
Will Gillette <wgillette02@gmail.com>
2023-03-02 01:02:45 -0500

    User authentication is complete besides the database aspects.

--- client/src/routes/ProtectedRoute.js
+++ client/src/routes/ProtectedRoute.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const ProtectedRoute = ({ isAuthenticated, dispatch }) => {console.log(isAuthenticated); return isAuthenticated ? <Outlet /> : <Navigate to={"/home"} replace />};
+const ProtectedRoute = ({ isAuthenticated, dispatch }) => isAuthenticated ? <Outlet /> : <Navigate to={"/home"} replace />;
 const mapStateToProps = (state) => ({ isAuthenticated: state.authReducer.authenticated });
 export default connect(mapStateToProps)(ProtectedRoute);
