f05d92212cb9dadf5185aac6ebcd37eecbb598bf
Will Gillette <wgillette02@gmail.com>
2023-03-27 09:52:23 -0400

    Started implementing database for removing and adding courses

--- server/app/controllers/planner.js
+++ server/app/controllers/planner.js
@@ -1,5 +1,4 @@
 // Planner.js Module: Handles changes to semester plans
-
 const { fetchDB } = require("../model");
 let COURSE_DATA = []
 const PLAN_TEMPLATE = {F2019: [], S2020: [], F2020: [], S2021: [], F2021: [], S2022: [], F2022: [], S2023: []}
@@ -13,21 +12,48 @@ exports.removeCourse = (req, res) => {
     console.log(courseId, semesterKey);
     // TO-DO: fill this in
     const userId = req.userId;
-    res.status(200).json({
-        fullPlan: {...TEST_TEMPLATE, [semesterKey]: [...TEST_TEMPLATE[semesterKey].filter((course) => course?.id !== courseId)]},
-        message: `Successfully removed course ${courseId} from ${semesterKey}!`
+    let query = 'DELETE FROM StudentCoursePlan WHERE studentId = ? AND courseId = ? AND semester = ?' 
+    db.run(query, [userId, courseId, semesterKey], (err) => {
+        if (err) {
+            res.status(500).json({message: err.message});
+            console.log(err);
+        } else {
+            res.status(200).json({
+                fullPlan: {}, // fullPlan: {...TEST_TEMPLATE, [semesterKey]: [...TEST_TEMPLATE[semesterKey].filter((course) => course?.id !== courseId)]},
+                message: `Successfully removed course ${courseId} from ${semesterKey}!`
+            })
+            console.log(`Deleted row ${this.lastID} with student ID: ${userId}, courseId: ${courseId}, and semester: ${semesterKey}`)
+        }
+    })
+    
+}
+
+const addCourseToSemester = (studentId, course, semester) => {
+    let query = 'INSERT INTO StudentCoursePlan (studentId, courseId, semester) VALUES (?, ?, ?)';
+    Promise((resolve, reject) => {
+        db.run(query, [studentId, course, semester], (err) => {
+            if (err) {
+                console.log(err);
+                reject(false)
+            } else {
+                console.log(`Inserted a row with the ID: ${this.lastID} - ${course} for ${semester}`)
+                resolve(true)
+            }
+        })
     })
 }
 
 exports.addCourses = (req, res) => {
     const [courseIdList, semesterKey] = Object.values(req.body);
-    // TO-DO: fill this in
     const userId = req.userId;
-    res.status(200).json({
-        // TO-DO: replace TEST_TEMPLATE with the user's plan data in the database and SAMPLE_COURSES with the full list of all courses
-        fullPlan: {...TEST_TEMPLATE, [semesterKey]: [...TEST_TEMPLATE[semesterKey], ...COURSE_DATA.filter((courseInfo) => courseIdList.includes(courseInfo?.id))]},
-        message: `Successfully added courses ${courseIdList.join(", ")} to ${semesterKey}!`
-    })
+    const promises = courseIdList.map((courseId) => addCourseToSemester(userId, courseId, semesterKey))
+    Promise.all(promises).then(() => {
+        res.status(200).json({
+            // TO-DO: replace TEST_TEMPLATE with the user's plan data in the database and SAMPLE_COURSES with the full list of all courses
+            fullPlan: {},//{...TEST_TEMPLATE, [semesterKey]: [...TEST_TEMPLATE[semesterKey], ...COURSE_DATA.filter((courseInfo) => courseIdList.includes(courseInfo?.id))]},
+            message: `Successfully added courses ${courseIdList.join(", ")} to ${semesterKey}!`
+        })
+    }).catch((err) => res.status(500).json({message: err.message}));
 }
 
 const getCourses = async () => {
@@ -81,6 +107,7 @@ exports.fetchPlan = async (req, res) => {
 
     COURSE_DATA = await fetchAllCourses();
     if (COURSE_DATA) {
+        // TO-DO: Replace TEST_TEMPLATE with the user's full course plan
         res.status(200).json({fullPlan: TEST_TEMPLATE, courseCatalog: COURSE_DATA});
     } else {
         res.status(500).json({message: "Failed to initialize course data."})
