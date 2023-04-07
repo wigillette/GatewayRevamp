6de6f69b2072818105726831543a55c1ac6795ff
Kace <lakacey03@gmail.com>
2023-04-07 11:57:59 -0400

    example test

--- server/app/controllers/__tests__/planner.js
+++ server/app/controllers/__tests__/planner.js
@@ -1,13 +1,18 @@
-// TODO create a mock DB and call commands from it,
-// and check the status to make sure the outputs are correct
+const planner_module = require("../planner")
 
-function sum(a, b) {
-  return a + b;
-}
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
 
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('fetchPlan', () => {
+  it('should fetch plan with status 200', async () => {
+    const mReq = { userId: 1 };    
+    const mRes = mockResponse()
+    await planner_module.fetchPlan(mReq, mRes)
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.json.mock.calls).toHaveLength(1)
+  });
 });
-
-
